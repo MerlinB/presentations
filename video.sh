b6b1ae47aee58a660d28c68ff578a63bb15fc5cf
Merlin <merlin.buczek@gmail.com>
2018-09-03 15:27:02 +0200

    font, audio output

--- video.sh
+++ video.sh
@@ -28,12 +28,13 @@ ffmpeg \
 		[c2][ovr1]overlay=W/2-w/2:H/3-h/2:shortest=1[out1];\
 		[out1][ovr2]overlay=0:0:shortest=1[out2]" \
 	-map "[out2]" -map 1:a \
-	intro_$TITLE.mp4
+  -c:v libx265 -preset fast \
+	Intro_$TITLE.mp4
 
 if [ -n "$PRESENTATION_IN" ] ; then
 	ffmpeg \
 		-y \
-		-t 60 -i $VIDEO_IN -ss $SS -i $PRESENTATION_IN -i intro_$TITLE.mp4 \
+		-t 60 -i $VIDEO_IN -ss $SS -i $PRESENTATION_IN -i Intro_$TITLE.mp4 \
 		-filter_complex "\
 			[1]crop=iw*0.96:ih*0.9:iw*0.02:ih*0.1[c];\
 			[c]scale=-1:1080[s];\
@@ -41,22 +42,24 @@ if [ -n "$PRESENTATION_IN" ] ; then
 			[s]pad=1920:1080:0:0[v];\
 			[0]scale=800:-1[p];\
 			[v][p]overlay=x=W-w:y=H-h:shortest=1[out];\
-			[i][2:a][out][0:a]concat=n=2:v=1:a=1[video][audio]" \
-		-map [video] -map [audio] \
-		-c:v libx265 -preset fast -c:a aac -b:a 192k \
-		$TITLE.mp4
+			[i][2:a][out][0:a]concat=n=2:v=1:a=1[video][audio];\
+      [audio]asplit[audio1][audio2]" \
+    -c:v libx265 -preset fast -c:a aac -b:a 192k \
+		-map [video] -map [audio1] $TITLE.mp4 \
+    -map [audio2] $TITLE.mp3
 else
 	ffmpeg \
 		-y \
-		-t 60 -i $VIDEO_IN -i intro_$TITLE.mp4 \
+		-t 60 -i $VIDEO_IN -i Intro_$TITLE.mp4 \
 		-filter_complex "\
 			[0]scale=-1:1080[s];\
 			[s]pad=1920:1080:ow/2-iw/2:0[v];\
 			[1]scale=1920:-1[i];\
-			[i][1:a][v][0:a]concat=n=2:v=1:a=1[video][audio]" \
-		-map [video] -map [audio] \
-		-c:v libx265 -preset fast -c:a aac -b:a 192k \
-		$TITLE.mp4
+			[i][1:a][v][0:a]concat=n=2:v=1:a=1[video][audio];\
+      [audio]asplit[audio1][audio2]" \
+      -c:v libx265 -preset fast -c:a aac -b:a 192k \
+  		-map [video] -map [audio1] $TITLE.mp4 \
+      -map [audio2] $TITLE.mp3
 fi
 
 
